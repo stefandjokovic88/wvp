a76f20c702eb5913e61311ab0b37ee85e1f20b7c
Stefan Djokovic <stefan.djokovic88@gmail.com>
2025-03-08 00:27:46 +0100

    first ver with all fields

--- script.js
+++ script.js
@@ -1,73 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("form");
-  const firstname = document.getElementById("firstname");
-  const lastname = document.getElementById("lastname");
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
+function sendMail() {
+  var params = {
+    firstname: document.getElementById("firstname").value,
+    lastname: document.getElementById("lastname").value,
+    email: document.getElementById("email").value,
+    mobile: document.getElementById("mobile").value,
+    profession: document.getElementById("profession").value,
+    height: document.getElementById("height").value,
+    weight: document.getElementById("weight").value,
+    healthrecord: document.getElementById("healthrecord").value,
+    idnumber: document.getElementById("idnumber").value,
+    idcard: document.getElementById("idcard").value,
+    city: document.getElementById("city").value,
+    accountnumber: document.getElementById("accountnumber").value,
+    notice: document.getElementById("notice").value,
+  };
 
-  //show input error messages
-  function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = "form-control error";
-    const small = formControl.querySelector("small");
-    small.innerText = message;
-  }
+  const serviceID = "service_viztgbn";
+  const templateID = "template_m366adu";
 
-  //error placeholders
-  function errorPlaceholder() {
-    if (firstname.value === "") {
-      firstname.placeholder = "";
-    }
-    if (lastname.value === "") {
-      lastname.placeholder = "";
-    }
-    if (email.value === "") {
-      email.placeholder = "email@example/com";
-      email.classList.add("error");
-    }
-    if (password.value === "") {
-      password.placeholder = "";
-    }
-  }
-
-  //show success colour
-  function showSucces(input) {
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
-  }
-
-  //check email is valid
-  function checkEmail(input) {
-    const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value.trim())) {
-      showSucces(input);
-    } else {
-      showError(input, "Looks like this is not an email");
-    }
-  }
-
-  //checkRequired fields
-  function checkRequired(inputArr) {
-    inputArr.forEach(function (input) {
-      if (input.value.trim() === "") {
-        showError(input, `${getFieldName(input)} cannot be empty`);
-      } else {
-        showSucces(input);
-      }
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("firstname").value = "";
+      document.getElementById("lastname").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("mobile").value = "";
+      document.getElementById("profession").value = "";
+      document.getElementById("height").value = "";
+      document.getElementById("weight").value = "";
+      document.getElementById("healthrecord").value = "";
+      document.getElementById("idnumber").value = "";
+      document.getElementById("idcard").value = "";
+      document.getElementById("city").value = "";
+      document.getElementById("accountnumber").value = "";
+      document.getElementById("notice").value = "";
+      // console.log(res);
+      alert("Podaci su uspešno poslati!");
+    })
+    .catch((err) => {
+      // console.log(err);
+      alert("Došlo je do greške prilikom slanja! Molimo pokušajte ponovo!");
     });
-  }
-
-  //get FieldName
-  function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-  }
-
-  //Event Listeners
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    errorPlaceholder();
-    checkRequired([firstname, lastname, email, password]);
-    checkEmail(email);
-  });
-});
+}
